680033ff48f5b589c43a761d0d43fddf526477ee
veld <1091939@bk.ru>
2023-03-02 13:33:22 +0300

    dz

---
+++ dz2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+ Console.Write("Введите первое число: ");
+ int number1 = Convert.ToInt32(Console.ReadLine());
+
+ Console.Write("Введите второе число: ");
+ int number2 = Convert.ToInt32(Console.ReadLine());
+
+ Console.Write("Введите третье число: ");
+ int number3 = Convert.ToInt32(Console.ReadLine());
+
+ int max;
+
+ if (number1 > number2 && number1 > number3)
+        {
+            Console.Write($"{max = number1}"   );
+        }
+ else if (number2 > number1 && number2 > number3)
+        {
+            Console.Write($"{max = number2} ");
+        }
+
+ else if (number3 > number1 && number3 > number2)
+        {
+            Console.Write($"{max = number3} ");
+         }
+    
---
+++ dz4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int temp = 2;
+
+while(temp <= N)
+{
+    Console.Write($"{temp} ");
+    temp = temp + 2;
+}
